a28399a1258100b67f4636d6caf5e0568b5afd46
Mohammad Sadil Khan <45021067+SadilKhan@users.noreply.github.com>
2022-03-02 20:47:21 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,21 +6,14 @@ import matplotlib.pyplot as plt
 
 SEG_MASK=np.array(os.listdir("/home/khan/Internship/Dataset/visceral/segmentations"))
 
-radLexIDDict={1247:"trachea",1302:"right lung",1326:"left lung",170:" pancreas",
-187:"gallbladder",237:"urinary bladder",2473: "sternum",29193:"firdt lumber vertebra",
-29662:"right kidney",29663:"left kidney",30324:"right adrenal gland",
-30325:"left adrenal gland",32248:"right psoas major",32249:"left psoas major",
-40357:"right rectus abdominis",40358: "left rectus abdominis",480:"aorta",
-58: "liver",7578:"thyroid gland",86:"spleen"}
-
-ORGAN_CHOICE={"liver":1,"pancreas":2,"spleen":3,"right lung":4,"left lung":5,"left kidney":6,"right kidney":7}
-
-def find_segmentation_mask(patNum):
+def find_segmentation_mask(segDir,patNum):
     """ Finds all the segmentation masks according to Patient Id"""
+    
+    # Get all the segmentation names
+    segMaskNames=os.listdir(segDir)
 
-    global allSegMask
     # Split the segmentation mask strings
-    allSegMaskSpl=[i.split("_") for i in allSegMask]
+    allSegMaskSpl=[i.split("_") for i in segMaskNames]
     
     # The first item is the patient number
     allIndex=[i for i in range(len(allSegMaskSpl)) if allSegMaskSpl[i][0]==str(patNum)]
