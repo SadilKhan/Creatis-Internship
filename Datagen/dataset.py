315609a31d0b4c50bde40fb1dbd27ad5ad2db91e
Mohammad Sadil Khan <45021067+SadilKhan@users.noreply.github.com>
2022-03-03 19:30:33 +0100

    Update dataset.py

--- Datagen/dataset.py
+++ Datagen/dataset.py
@@ -1,3 +1,4 @@
+from re import S
 import numpy as np
 import pandas as pd
 import os
@@ -13,7 +14,32 @@ class PointCloudDataset(Dataset):
     """ Custom Class for Point Cloud Dataset """
 
     def __int__(self,csvDir,transforms=False):
-        pass
+        self.csvDir = csvDir
+        self.csvFiles=os.listdir(self.csvDir)
+        self.transforms=transforms
+
+    def __len__(self):
+        return len(self.csvFiles)
 
     def __getitem__(self, index):
-        pass
+        csvData=pd.read(self.csvDir+"/"+self.csvFiles[0])
+
+        pointSet=torch.from_numpy(csvData[["x","y","z","value","magnitude"]].values)
+
+        # Normalize to Unit Ball
+        pointSet[:,:3]-=torch.mean(pointSet[:, :3], axis=0)
+        pointSet[:,:3]/=torch.max(np.sqrt(torch.sum(pointSet[:, :3] ** 2, axis=1)), 0)
+
+        # Scale the remaining features
+        pointSet[:,3:]-=torch.mean(pointSet[:, 3:], axis=0)
+        pointSet[:,3:]/=torch.var(pointSet[:,3:],axis=0)    
+
+        # Labelling
+        label=torch.tensor([ORGAN_TO_LABEL[l] for l in csvData["label"]])
+
+        if self.transforms:
+            #pointSet[:,3:]=random_scale(pointSet[:,])
+            #pointSet[:,3:]=translate_pointcloud(pointSet[:,])
+            pass
+
+        return pointSet,label            
