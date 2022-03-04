d2d605a2301c5b18890295053f7e202fef2cdbda
Mohammad Sadil Khan <45021067+SadilKhan@users.noreply.github.com>
2022-03-04 14:07:58 +0100

    Update model_utils.py

--- Model/model_utils.py
+++ Model/model_utils.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import sys
+import numpy as np
+from sklearn.metrics import confusion_matrix
 
 
 def square_distance(src, dst):
@@ -86,15 +88,85 @@ def fps(xyz, M):
     B, N, C = xyz.shape
     centroids = torch.zeros(size=(B, M), dtype=torch.long).to(device)
     dists = torch.ones(B, N).to(device) * 1e5
+    print(dists.shape)  
     inds = torch.randint(0, N, size=(B, ), dtype=torch.long).to(device)
     batchlists = torch.arange(0, B, dtype=torch.long).to(device)
     for i in range(M):
         centroids[:, i] = inds
         cur_point = xyz[batchlists, inds, :] # (B, 3)
         cur_dist = torch.squeeze(get_dists(torch.unsqueeze(cur_point, 1), xyz))
+        print(cur_dist.shape)
         dists[cur_dist < dists] = cur_dist[cur_dist < dists]
         inds = torch.max(dists, dim=1)[1]
     return centroids
+
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author: Yue Wang
+@Contact: yuewangx@mit.edu
+@File: util
+@Time: 4/5/19 3:47 PM
+"""
+
+
+
+def cal_loss(pred, label, smoothing=True):
+    """
+    Calculate cross entropy loss, apply label smoothing if needed.
+    """
+    label = label.contiguous().view(-1)  # [Batch_size]
+    predLabel=pred.detach().cpu().argmax(axis=1)
+
+    iou=compute_iou(predLabel,label.cpu())
+
+    if smoothing:
+        eps = 0.2
+        n_class = pred.size(1)
+
+        one_hot = torch.zeros_like(pred).scatter(1, label.view(-1, 1), 1)
+        one_hot = one_hot * (1 - eps) + (1 - one_hot) * eps / (n_class - 1)
+        log_prb = F.log_softmax(pred, dim=1)
+
+        loss = -(one_hot * log_prb).sum(dim=1).mean()
+    else:
+        loss = F.cross_entropy(pred, label, reduction='mean')
+
+    return loss,iou
+
+
+class IOStream:
+    def __init__(self, path):
+        self.f = open(path, 'a')
+
+    def cprint(self, text):
+        print(text)
+        self.f.write(text + '\n')
+        self.f.flush()
+
+    def close(self):
+        self.f.close()
+
+
+def compute_iou(y_pred, y_true):
+    # ytrue, ypred is a flatten vector
+    y_pred = y_pred.flatten()
+    y_true = y_true.flatten()
+    current = confusion_matrix(y_true, y_pred, labels=[0, 1])
+    # compute mean iou
+    intersection = np.diag(current)[1]
+    ground_truth_set = current.sum(axis=1)[1]
+    predicted_set = current.sum(axis=0)[1]
+    union = ground_truth_set + predicted_set - intersection
+    if union>0:
+        IoU = intersection / union.astype(np.float32)
+    else:
+        IoU=0
+
+    return IoU
+
+
 
 
 
