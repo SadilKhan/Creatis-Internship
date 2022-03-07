fcd1c03d2ddb3b6dfa37a4b6f98a4a352371a4a5
Mohammad Sadil Khan <45021067+SadilKhan@users.noreply.github.com>
2022-03-07 17:31:18 +0100

    fixed colors for organs

--- Datagen/visualize.py
+++ Datagen/visualize.py
@@ -3,39 +3,50 @@ import pandas as pd
 import numpy as np
 import argparse
 
+from globalVar import ORGAN_TO_LABEL,ORGAN_TO_RGB
 
-def visualize_point_cloud(csvPath):
+
+def visualize_point_cloud(csvPath,label="all"):
     """ 3d visualization of point clouds """
 
     pointCloud=pd.read_csv(csvPath)
+    if label=="no_background":
+        pointCloud=pointCloud[(pointCloud['label']!="Background")]
+    elif label!="all":
+        pointCloud=pointCloud[pointCloud['label']==label]
     unique_label=pointCloud["label"].unique()
-    labelDict=dict()
+    """labelDict=dict()
     colorDict=dict()
     # Make colors for every unique label
     for l in unique_label:
-        colorDict[l]=np.random.choice(range(256), size=3)
         labelDict[l]=list(colorDict[l]/255.0)
-    print("Color Code",colorDict)
+    print("Color Code",colorDict)"""
 
     # Make color lists
-    colors=np.array([labelDict[l] for l in pointCloud["label"]])
+    colors=np.array([ORGAN_TO_RGB[l] for l in pointCloud["label"]])
 
     # open3d visualization
     pcd=o3d.geometry.PointCloud()
     pcd.points=o3d.utility.Vector3dVector(pointCloud[["x","y","z"]].values)
-    pcd.colors=o3d.utility.Vector3dVector(colors)  
+    pcd.colors=o3d.utility.Vector3dVector(colors)
 
-    o3d.visualization.draw_geometries([pcd])
+    o3d.visualization.draw_geometries_with_animation_callback([pcd],
+                                                              rotate_view)
 
+def rotate_view(vis):
+    ctr = vis.get_view_control()
+    ctr.rotate(1.0, 0.0)
+    return False
 
 def main():
     parser=argparse.ArgumentParser()
 
     parser.add_argument("--csvPath",help="CSV Path for point cloud",required=True)
+    parser.add_argument("--label",help="which label to show",default="all")
 
     args=parser.parse_args()
 
-    visualize_point_cloud(args.csvPath)
+    visualize_point_cloud(args.csvPath,args.label)
 
 
 
