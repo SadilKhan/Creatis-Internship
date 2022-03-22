9fcada72bace8224fffc3ef9747afe0f52dc6b9a
Mohammad Sadil Khan <45021067+SadilKhan@users.noreply.github.com>
2022-03-22 15:44:02 +0100

    Update globalVar.py

--- Datagen/globalVar.py
+++ Datagen/globalVar.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-
+#! FOR VISCERAL DATASET
 radLexIDDict={1247:"trachea",1302:"right lung",1326:"left lung",170:" pancreas",
 187:"gallbladder",237:"urinary bladder",2473: "sternum",29193:"firdt lumber vertebra",
 29662:"right kidney",29663:"left kidney",30324:"right adrenal gland",
@@ -9,11 +9,35 @@ radLexIDDict={1247:"trachea",1302:"right lung",1326:"left lung",170:" pancreas",
 40357:"right rectus abdominis",40358: "left rectus abdominis",480:"aorta",
 58: "liver",7578:"thyroid gland",86:"spleen"}
 
-ORGAN_CHOICE={"liver":58,"pancreas":170,"spleen":86,"gallbladder":187,"urinary bladder":237,"left kidney":29663,"right kidney":29662}
+ORGAN_CHOICE={"liver":58,"right_lung":1302,"left_lung":1326,
+"bladder":237,"left_kidney":29663,"right_kidney":29662}
 
-ORGAN_TO_LABEL={"background":0,"liver":1,"pancreas":2,"spleen":3,"gallbladder":4,"urinary bladder":5,"left kidney":6,"right kidney":7}
+# ! FOR CT ORG DATASET
+# Before distinguishing the two kidneys
+CTORG_LABEL_TO_ORGAN={0: "background",
+1: "liver",
+2: "bladder",
+3: "lungs",
+4: "kidneys",
+5: "bone",
+6: "brain"}
+
+# After distinguishing the two kidneys
+CTORG_ORGAN_CHOICE={"background":0,
+"liver":1,
+"bladder":2,
+"lungs":3,
+"left_kidney":4,
+"right_kidney":7}
+
+
+# ! GLOBAL VARIABLES
+ORGAN_TO_LABEL={"background":0,"liver":1,"lungs":2,
+"bladder":3,"left_kidney":4,"right_kidney":5}
+
+LABEL_TO_ORGAN={0:"background",1:"liver",2:"lungs",
+3:"bladder",4:"left_kidney",5:"right_kidney"}
 
 ORGAN_TO_RGB={"background":np.array([130,  15, 178])/255.0, "liver":np.array([232, 72, 80])/255.0,
-"pancreas":np.array([190, 254, 249])/255.0,"spleen":np.array([126,   3,  22])/255.0,
-"gallbladder":np.array([124,  87, 238])/255.0,"urinary bladder":np.array([27, 7, 65])/255.0,
-"left kidney":np.array([123, 225, 193])/255.0,"right kidney":np.array([ 77, 220,  34])/255.0}
+"lungs":np.array([190, 254, 249])/255.0,"bladder":np.array([27, 7, 65])/255.0,
+"left_kidney":np.array([79, 116, 172])/255.0,"right_kidney":np.array([79, 152, 172])/255.0}
