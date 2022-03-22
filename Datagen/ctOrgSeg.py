6b276f57c60eeb3a6c02b52f26760b95e47f4af2
Mohammad Sadil Khan <45021067+SadilKhan@users.noreply.github.com>
2022-03-22 15:43:40 +0100

    bug fixes

--- Datagen/ctOrgSeg.py
+++ Datagen/ctOrgSeg.py
@@ -4,45 +4,47 @@ from torch import threshold
 from utils import transform_to_ras
 import argparse
 import os
+from tqdm import tqdm
 
-def corSegLabel(segFile):
+def corSegLabel(segFile,threshold):
     """ Function for creating labels from kidney to right and left kidney """
-    seg=transform_to_ras(segFile)
+    seg=nib.load(segFile)
     segData=seg.get_fdata()
+    segData=np.ceil(segData)
     
     # Find positions for kidney
-    xk,yk,zk=(segData>0).nonzero()
+    xk,yk,zk=(segData==4).nonzero()
     
     # Find left right kidney positions
-    threshold=10 # threshold for the distance between two consecutive positions
+    # threshold for the distance between two consecutive positions
     for i in range(1,len(xk)):
         if np.abs(xk[i]-xk[i-1])>threshold:
-            break
+            break    
     # The positions for the right kidney
     xkr,ykr,zkr=xk[i:],yk[i:],zk[i:]
 
     for i in range(len(xkr)):
-        segData[xkr[i],ykr[i],zkr[i]]=6
-    segName=segFile.split("/")[-1].split(".")
+        segData[xkr[i],ykr[i],zkr[i]]=7
+    """segName=segFile.split("/")[-1].split(".")
     segName[0]+="corrected"
     segName="/".join(segFile.split("/")[:-1])+"/"+".".join(segName)
-    
-    nib.save(nib.Nifti1Image(segData),segName)
+    """
+    nib.save(nib.Nifti1Image(segData,seg.affine),segFile)
     
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--segDir",help="The directory containing segmentation masks")
+    parser.add_argument("--segDir",help="The directory containing segmentation masks",required=True)
+    parser.add_argument("--threshold",help="threshold for the distance between two consecutive positions",type=float,default=10)
 
     args=parser.parse_args()
 
     segDir=args.segDir
     segMasks=os.listdir(segDir)
 
-    for msk in segMasks:
+    for msk in tqdm(segMasks):
         if "labels" in msk:
-            corSegLabel(segDir+"/"+msk)
+            corSegLabel(segDir+"/"+msk,args.threshold)
 
 
 if __name__ == "__main__":
     main()
-
