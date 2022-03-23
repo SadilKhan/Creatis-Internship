862c98b3600fcb1d4c8b9b2fd963b64df3500fdc
Mohammad Sadil Khan <45021067+SadilKhan@users.noreply.github.com>
2022-03-23 16:06:53 +0100

    directory issue fix

--- Datagen/canny.py
+++ Datagen/canny.py
@@ -3,6 +3,8 @@ import itk
 from tqdm import tqdm
 import argparse
 
+from utils import *
+
 
 def canny(inputImage,outputImage,variance,lowerT,upperT):
 
@@ -40,6 +42,7 @@ def canny(inputImage,outputImage,variance,lowerT,upperT):
 
 def main():
     parser = argparse.ArgumentParser(description="Positional Arguments for Canny Edge Detection")
+    parser.add_argument('--datatype',type=str,help="visceral or ctorg",required=True)
     parser.add_argument('--inputDir',type=str,help="Image Directory",required=True)
     parser.add_argument('--outputDir',type=str,help="Output Directory",required=True)
     parser.add_argument('--variance',type=list,nargs=3,default=[1,1,1])
@@ -47,13 +50,18 @@ def main():
     parser.add_argument('--upperT',type=float,default=50,help="Upper threshold for Edge Detection")
 
     args=parser.parse_args()
-
-    inputImages=os.listdir(args.inputDir)
-    
+    if args.datatype=="ctorg":
+        inputImages=ctorg_find_volume_mask(args.inputDir)
+    else:
+        inputImages=os.listdir(args.inputDir)
     for img in tqdm(inputImages):
-        outImage=args.outputDir+"/"+img.split("/")[-1]
+        if args.datatype=="ctorg":
+            outImageName=img.split(".")[0]+f"_edge_{args.lowerT}_{args.upperT}"+".nii.gz"
+        else:
+            outImageName=img.split("/")[-1].split(".")[0]+f"_edge_{args.lowerT}_{args.upperT}"+".nii.gz"
+        outImagePath=args.outputDir+"/"+outImageName
          # Edge Detection
-        canny(args.inputDir+"/"+img,outImage,args.variance,args.lowerT,args.upperT)
+        canny(args.inputDir+"/"+img,outImagePath,args.variance,args.lowerT,args.upperT)
 
 
    
