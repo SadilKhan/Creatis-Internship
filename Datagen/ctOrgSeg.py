93260d66ca3cf3ec30a4bc6bac9b07dc6d095900
Mohammad Sadil Khan <45021067+SadilKhan@users.noreply.github.com>
2022-03-25 15:34:00 +0100

    bug fixes
    - instead of using consecutive points along x_axis, this version uses a bounding cube to find a hyperplane to distinguish kidney labels

--- Datagen/ctOrgSeg.py
+++ Datagen/ctOrgSeg.py
@@ -4,9 +4,10 @@ from torch import threshold
 from utils import ctorg_find_seg_mask, transform_to_ras
 import argparse
 import os
+from utils import *
 from tqdm import tqdm
 
-def corSegLabel(segFile,threshold):
+def corSegLabel(segFile):
     """ Function for creating labels from kidney to right and left kidney """
     seg=nib.load(segFile)
     segData=seg.get_fdata()
@@ -14,35 +15,33 @@ def corSegLabel(segFile,threshold):
     
     # Find positions for kidney
     xk,yk,zk=(segData==4).nonzero()
-    
+
+    # Create bounding cube
+    A0,A1=find_cube(segData,label=4)
+
+    hyperplane=(A0+A1)/2
+
     # Find left right kidney positions
-    # threshold for the distance between two consecutive positions
-    for i in range(1,len(xk)):
-        if np.abs(xk[i]-xk[i-1])>threshold:
-            break    
+    x_index=(xk<hyperplane[0]).nonzero()[0].max()
+    
     # The positions for the right kidney
-    xkr,ykr,zkr=xk[i:],yk[i:],zk[i:]
+    xkr,ykr,zkr=xk[x_index+1:],yk[x_index+1:],zk[x_index+1:]
 
     for i in range(len(xkr)):
         segData[xkr[i],ykr[i],zkr[i]]=7
-    """segName=segFile.split("/")[-1].split(".")
-    segName[0]+="corrected"
-    segName="/".join(segFile.split("/")[:-1])+"/"+".".join(segName)
-    """
+    
     nib.save(nib.Nifti1Image(segData,seg.affine),segFile)
     
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--segDir",help="The directory containing segmentation masks",required=True)
-    parser.add_argument("--threshold",help="threshold for the distance between two consecutive positions",type=float,default=10)
-
     args=parser.parse_args()
 
     segDir=args.segDir
     segMasks=ctorg_find_seg_mask(segDir)
 
     for msk in tqdm(segMasks):
-        corSegLabel(segDir+"/"+msk,args.threshold)
+        corSegLabel(segDir+"/"+msk)
 
 
 if __name__ == "__main__":
