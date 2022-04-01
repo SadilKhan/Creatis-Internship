f57a1aaa5b8c9a0bdc8be29e40897d4aa48b2aed
Mohammad Sadil Khan <45021067+SadilKhan@users.noreply.github.com>
2022-04-01 16:14:40 +0200

    no segmentation resizing

--- Datagen/resize.py
+++ Datagen/resize.py
@@ -14,17 +14,7 @@ class ResizeImage:
     def __init__(self,datatype,imagePath,segPath,resizeRate):
         self.datatype = datatype
         self.imagePath = imagePath
-        self.segPath = segPath
         self.resizeRate = resizeRate
-
-        if self.datatype == "visceral":
-            self.Num = self.imagePath.split("/")[-1].split("_")[0]
-            allSegName= find_segmentation_mask(self.segPath,self.Num)
-            self.allSegPath=[self.segPath+"/"+i for i in allSegName]
-        else:
-            self.Num = self.imagePath.split("/")[-1].split("_")[0].split("-")[-1]
-            allSegName= ctorg_find_seg_mask(self.segPath,self.Num)
-            self.allSegPath=[self.segPath+"/"+i for i in allSegName]
 
         self.resize()
     
@@ -36,20 +26,11 @@ class ResizeImage:
         rescaledImageData=zoom(imageData, self.resizeRate)
         nib.save(nib.Nifti1Image(rescaledImageData,image.affine),self.imagePath)
 
-        # Resize the Segmentation
-        for path in tqdm(self.allSegPath):
-            #print(path)
-            seg=nib.load(path)
-            segData=seg.get_fdata()
-            rescaledSegData=zoom(segData, self.resizeRate)
-            nib.save(nib.Nifti1Image(rescaledSegData,seg.affine),path)
-
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--datatype",help="ctorg or visceral",type=str,default="visceral")
     parser.add_argument("--inputDir",help="Image Directory",type=str,required=True)
-    parser.add_argument("--segDir",help="Segmentation Directory",type=str,required=True)
     parser.add_argument("--resizeRate",help="Resize rate",type=float,nargs=3,default=[0.25,0.25,0.25])
 
     args=parser.parse_args()
@@ -57,7 +38,7 @@ def main():
     allImagePath=glob.glob(os.path.join(args.inputDir,"*"))
 
     for path in tqdm(allImagePath):
-        ResizeImage(args.datatype,path,args.segDir,args.resizeRate)
+        ResizeImage(args.datatype,path,args.resizeRate)
             
 
 
